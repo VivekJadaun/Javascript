ab353f5d311536f84e534adbbfb1d5de20b967aa
Vivek Jadaun <vivek@vinsol.com>
2018-12-10 11:20:14 +0530

    Anchors & iFrames yet to be handled

--- eraser.js
+++ eraser.js
@@ -1,39 +1,54 @@
-function Eraser() {}
-
-Eraser.prototype.init = function() {
-  this.elementHighlighter();
-  this.bindEraser();
-};
-
-Eraser.prototype.elementHighlighter = function() {
-  window.addEventListener('mouseover', function (event) {
-    // console.log(event.type, event.target);
-    event.target.style.border = '1.5px solid black';
-  });
-
-  window.addEventListener('mouseout', function (event) {
-    // console.log(event.type, event.target);
-    event.target.style.border = 'none';
-  });
-};
-
-Eraser.prototype.bindEraser = function() {
-  var _this = this;
-  window.addEventListener('click', function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-    _this.makeTargetGoPoof(event);
-  });
-};
-
-Eraser.prototype.makeTargetGoPoof = function(event) {
-  event.target.style.display = 'none';
-};
-
-
-window.addEventListener('load', function () {
-  new Eraser().init();
-});
+// function Eraser() {}
+//
+// Eraser.prototype.elementInFocus = null;
+//
+// Eraser.prototype.init = function() {
+//   this.bindHighlighters();
+//   this.bindEraser();
+// };
+//
+// Eraser.prototype.bindHighlighters = function() {
+//   window.addEventListener('mouseover', this.emphasizer.bind(this));
+//   window.addEventListener('mouseout', this.deEmphasizer.bind(this));
+//   window.addEventListener('keydown', this.die.bind(this));
+//
+// };
+//
+// Eraser.prototype.emphasizer = function (event) {
+//   event.target.style.outline = '1.5px solid black';
+//   this.elementInFocus = event.target;
+// };
+//
+// Eraser.prototype.deEmphasizer = function () {
+//   event.target.style.outline = '';
+// };
+//
+// Eraser.prototype.bindEraser = function() {
+//   var _this = this;
+//   window.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     event.stopPropagation();
+//     event.stopImmediatePropagation();
+//     _this.makeTargetGoPoof(event);
+//   });
+// };
+//
+// Eraser.prototype.makeTargetGoPoof = function(event) {
+//   event.target.style.display = 'none';
+// };
+//
+// Eraser.prototype.die = function (event) {
+//   if (event.keyCode === 27 || event.which === 27) {
+//     window.removeEventListener('mouseover', this.emphasizer);
+//     window.removeEventListener('mouseout', this.deEmphasizer);
+//     this.elementInFocus.style.outline = '';
+//     // debugger
+//   }
+// };
+//
+// window.addEventListener('load', function () {
+//   new Eraser().init();
+// });
 
 
 
@@ -42,25 +57,34 @@ window.addEventListener('load', function () {
 
 
 javascript:(function() {
+  var elementInFocus = null;
   window.addEventListener('click', eraser);
-  window.addEventListener('mouseover', highlighter);
-  window.addEventListener('mouseout', highlighter);
-  window.addEventListener('keydown', function(event) {
+  window.addEventListener('mouseover', emphasizer);
+  window.addEventListener('mouseout', deEmphasizer);
+  window.addEventListener('keydown', existNoMore);
+  function existNoMore(event) {
     if (event.keyCode === 27 || event.which === 27) {
-      // console.log(event);
-      window.removeEventListener('keydown', eraser);
-      window.removeEventListener('keydown', highlighter);
-    }
-  }); 
-  function highlighter (event) {
-    if (event.target.style.border === 'none') {
-      event.target.style.border = '1.5px solid red';
-    } else {
-      event.target.style.border = 'none';
+      window.removeEventListener('click', eraser);
+      window.removeEventListener('mouseover', emphasizer);
+      window.removeEventListener('mouseout', deEmphasizer);
+      window.removeEventListener('keydown', existNoMore);
+      elementInFocus.style.outline = '';
     }
   }
+  function emphasizer(event) {
+    event.target.style.outline = '1.5px solid #e91010';
+    elementInFocus = event.target;
+  }
+  function deEmphasizer(event) {
+    event.target.style.outline = '';
+  }
   function eraser(event) {
+    if (event.target.tagName === 'A' || event.target.tagName === 'a') {
+      debugger
+      return false;
+    }
     event.preventDefault();
+    event.stopImmediatePropagation();
     event.stopPropagation();
     event.target.style.display = 'none';
   }
